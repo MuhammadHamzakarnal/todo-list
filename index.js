4f174282fcaf8fb6926e7bb4bb4b42783ada48e3
MuhammadHamzakarnal <160274992+MuhammadHamzakarnal@users.noreply.github.com>
2024-04-15 17:38:24 +0500

    push my todo-list to github

---
+++ index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let todosQus = await inquirer.prompt([
+        {
+            name: "firstQuestion",
+            type: "input",
+            message: "what would like to add in you todos?",
+        },
+        {
+            name: "secondQuestion",
+            type: "confirm",
+            message: "would you like to add more in your todos?",
+            default: "true"
+        }
+    ]);
+    todos.push(todosQus.firstQuestion);
+    console.log(todos);
+    //The loop is running on the based of this variable condition
+    condition = todosQus.secondQuestion;
+}
+//home work read , update, delete, add
